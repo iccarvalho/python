bfe72e6f44a980bd727964ae8ff3c6dcb209c2b3
iccarvalho <igccarvalho06@outlook.com>
2025-09-12 19:39:13 -0300

    elif e else

--- condicionais.py
+++ condicionais.py
@@ -7,5 +7,9 @@ print('fim do programa')
 
 nota = int(input('Informe sua nota: '))
 
-if(nota >= 30 and nota < 60):
+if(nota >= 60):
+    print(f'Você passou!')
+elif(nota >= 30 and nota < 60):
     print(f'Você precisa fazer sub, faz o L')
+else:
+    print(f'Você reprovou :(')
