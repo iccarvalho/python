4548046844fefa23bfd50d1f61310d5061c552a2
iccarvalho <igccarvalho06@outlook.com>
2025-10-24 19:26:31 -0300

    metodos de listas

---
+++ aula003/listas3.py
@@ -0,0 +1,31 @@
+# Metodos de Listas
+
+# .append() adiciona um elemento no final da lista
+
+num = [1,2,3,5,8,4]
+num.append(15)
+print(num)
+
+# .copy() faz uma cópia da lista
+
+a = [1,6,1,8,2]
+b = a.copy()
+print(b)
+
+# .reverse() inverte a lista
+
+num = [1,2,3,4,5]
+num.reverse()
+print(num)
+
+# .sort() Ordena a lista
+
+num = [5,1,3,6,9,7]
+num.sort()
+print(num) 
+
+# .pop() remove o elemento no índice indicado
+
+num = [1,2,3,4,5]
+num.pop()
+print(num) # remove o 5
