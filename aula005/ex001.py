e3f7609a4ef82b687f00a16b17ef1a7b7a893cdd
iccarvalho <igccarvalho06@outlook.com>
2025-11-07 17:37:16 -0300

    arrumando ex001

--- aula005/ex001.py
+++ aula005/ex001.py
@@ -9,48 +9,58 @@
     Calcule a rentabilidade ano a ano e total.
     OBS: Para CDB há imposto de renda sobre o rendimento de 15%
 """
-rendimentos = []
-def cdb(valor, prazo, juros = 0.145):
-    for i in range(1, prazo+1):
-        valor += valor * juros
-        rendimentos.append(round(valor, 2))
-    mostrarRent(valor, prazo)
+
+def main():
+    investimento = float(input("Informe o valor que será investido: "))
+    while(True):
+        prazo = int(input("Escolha o prazo em anos [5, 10 , 20]: "))
+        if prazo == 5 or prazo == 10 or prazo == 20:
+            break
+        else:
+            print("Prazo Inválido! Tente novamente.")
+
+    while(True):
+        aplicacao = int(input("Escolha uma aplicação:\n1 - CDB\n2 - LCA\n3 - Poupança\n"))
+        match aplicacao:
+            case 1:
+                cdb(investimento, prazo)
+                break
+            case 2:
+                lca(investimento, prazo)
+                break
+            case 3:
+                poupanca(investimento, prazo)
+                break
+            case _:
+                print("Aplicação inválida!")
+
+def cdb(valor, prazo, juros = 0.145, IR = 0.85):
+    rendimentos = []
+    for _ in range(1, prazo+1):
+        valor += (valor * juros) * IR
+        rendimentos.append(valor)
+
+    for i, rendimento in enumerate(rendimentos, start=1):
+        print(f"Ano {i}: R$ {rendimento:.2f}")
 
 
 def lca(valor, prazo, juros = 0.115):
-    for i in range(1, prazo+1):
+    rendimentos = []
+    for _ in range(1, prazo+1):
         valor += valor * juros
-        rendimentos.append(round(valor, 2))
-    mostrarRent(valor, prazo)
+        rendimentos.append(valor)
+    
+    for i, rendimento in enumerate(rendimentos, start=1):
+        print(f"Ano {i}: R$ {rendimento:.2f}")
 
 
 def poupanca(valor, prazo, juros = 0.06):
-    for i in range(1, prazo+1):
+    rendimentos = []
+    for _ in range(1, prazo+1):
         valor += valor * juros
-        rendimentos.append(round(valor, 2))
-    mostrarRent(valor, prazo)
+        rendimentos.append(valor)
+    
+    for i, rendimento in enumerate(rendimentos, start=1):
+        print(f"Ano {i}: R$ {rendimento:.2f}")
 
-def mostrarRent(rendimento, prazo):
-    for i in range(1, prazo+1):
-        print(f"Ano {i}: {rendimento[i]}")
-
-investimento = float(input("Informe o valor que será investido: "))
-while(True):
-    prazo = int(input("Escolha o prazo em anos [5, 10 , 20] :"))
-    if prazo == 5 or prazo == 10 or prazo == 20:
-        break
-while(True):
-    aplicacao = int(input("Escolha uma aplicação:\n1 - CDB\n2 - LCA\n3 - Poupança\n"))
-    match aplicacao:
-        case 1:
-            cdb(investimento, prazo)
-            break
-        case 2:
-            lca(investimento, prazo)
-            break
-        case 3:
-            poupanca(investimento, prazo)
-            break
-        case _:
-            print("Aplicação inválida!")
-
+main()
